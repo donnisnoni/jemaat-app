5f3b8d5f7d689b77513d791dc5f4ff8b042168d5
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-12-23 06:25:33 +0800

    Return back to the code from svelte bug reprodution

--- src/store/rayon.js
+++ src/store/rayon.js
@@ -1,5 +1,5 @@
 // import store from './store';
-import { writable } from 'svelte/store';
+// import { writable } from 'svelte/store';
 import cache from './cache';
 import http from '/@shared/http';
 import axios from 'axios';
@@ -16,21 +16,19 @@ let cancelTokenSrc = axios.CancelToken.source();
 // export async function get() {
 //   return store
 // }
-const url = '/test.json';
+const url = '/api/data/rayon';
 
 /** Trigger fetch data from server */
 export async function fetch() {
-  // if (cache.has(url)) {
-  //   return cache.get(url);
-  // } else {
-  cancelTokenSrc = axios.CancelToken.source();
-  return http.get(url, { cancelToken: cancelTokenSrc.token }).then((response) => {
-    // cache.set(url, response.data);
-    // setTimeout(() => {
-    return Promise.resolve(response.data);
-    // }, 3000);
-  });
-  // }
+  if (cache.has(url)) {
+    return cache.get(url);
+  } else {
+    cancelTokenSrc = axios.CancelToken.source();
+    return http.get(url, { cancelToken: cancelTokenSrc.token }).then((response) => {
+      cache.set(url, response.data);
+      return Promise.resolve(response.data);
+    });
+  }
 }
 
 /** Delete the cache */
