b2d3cccebad5e5043ffda4196e94a581a2df6a3f
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-12-23 22:36:37 +0800

    Optimize code: Using conditional check instead of  using JSON.parse()

--- src/views/admin/Sidenav.svelte
+++ src/views/admin/Sidenav.svelte
@@ -2,11 +2,8 @@
   import { link } from 'svelte-spa-router';
   import active from 'svelte-spa-router/active';
 
-  let hidden = false;
-
-  let expanded =
-    JSON.parse(localStorage.getItem('admin-sidenav')) ||
-    localStorage.setItem('admin-sidenav', false);
+  let hidden = false,
+    expanded = localStorage.getItem('admin-sidenav') == 'true' || localStorage.setItem('admin-sidenav', false);
 
   function toggleExpand() {
     expanded = !expanded;
@@ -59,26 +56,19 @@
         role="button"
         tabindex="0"
         title={expandButtonCaption}>
-        <i
-          class="mdi mdi-chevron-double-right"
-          class:mdi-rotate-180={expanded} />
+        <i class="mdi mdi-chevron-double-right" class:mdi-rotate-180={expanded} />
         <span class="link-label">{expandButtonCaption}</span>
       </a>
     </li>
     <li class="md:hidden">
       <!-- svelte-ignore a11y-missing-attribute -->
-      <a
-        class="cursor-pointer select-none"
-        on:click={() => (hidden = true)}
-        role="button">
+      <a class="cursor-pointer select-none" on:click={() => (hidden = true)} role="button">
         <i class="mdi mdi-chevron-double-left" />
         <span class="link-label">Sembunyikan Menu</span>
       </a>
     </li>
     <li>
-      <a href="/" title="Logout" use:link><i class="mdi mdi-logout" />
-        <span class="link-label">Logout</span>
-      </a>
+      <a href="/" title="Logout" use:link><i class="mdi mdi-logout" /> <span class="link-label">Logout</span> </a>
     </li>
   </ul>
 </nav>
