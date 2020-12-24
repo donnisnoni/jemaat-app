0c39a0844ee71bfd2c150a508f6aaeae74b511c6
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-12-24 17:57:36 +0800

    Add slide top animation

--- src/shared/animation.js
+++ src/shared/animation.js
@@ -3,4 +3,12 @@ export default {
     KEYFRAMES: [{ transform: 'scale(0)' }, { transform: 'scale(1)' }],
     EASING: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
   },
+  SLIDE_TOP: {
+    KEYFRAMES: [
+      //
+      { transform: 'translateY(-10px)', opacity: 0 },
+      { transform: 'translateY(0px)', opacity: 1 },
+    ],
+    EASING: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+  },
 };
