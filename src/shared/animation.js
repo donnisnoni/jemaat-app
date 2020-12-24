387ab7da5c75f2fdc8f7ed51a0d80779ff2689e2
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-12-25 03:11:41 +0800

    Add opacity to FADE_IN transition

--- src/shared/animation.js
+++ src/shared/animation.js
@@ -1,11 +1,13 @@
 export default {
   FADE_IN: {
-    KEYFRAMES: [{ transform: 'scale(0)' }, { transform: 'scale(1)' }],
+    KEYFRAMES: [
+      { opacity: 0, transform: 'scale(0)' },
+      { opacity: 1, transform: 'scale(1)' },
+    ],
     EASING: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
   },
   SLIDE_TOP: {
     KEYFRAMES: [
-      //
       { transform: 'translateY(-10px)', opacity: 0 },
       { transform: 'translateY(0px)', opacity: 1 },
     ],
