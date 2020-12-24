247631db11ddc4dd1158bf26aff8e0afa8e32c9b
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-12-25 05:24:58 +0800

    Load index.scss first

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import moment from 'moment';
 import momentID from './libs/momen-id';
 
 /* --------------------------------- STYLE'S -------------------------------- */
+import './styles/index.scss';
 import './styles/buttons.scss';
 import './styles/materialdesignicons.css';
-import './styles/index.scss';
 // import './styles/progress-circular.scss';
 import './styles/inputs.scss';
 import './styles/mdi.scss';
