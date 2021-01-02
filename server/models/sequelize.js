9e006c2dc9ceb25db2695561c7f99bc0b4bcb884
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-02 09:52:26 +0800

    Separate the instance so we can using sync

--- server/models/sequelize.js
+++ server/models/sequelize.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize({
+const instance = new Sequelize({
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_DB || 'jemaat',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   dialect: 'mariadb',
 })
+
+// instance.sync({ alter: true })
+
+module.exports = instance
