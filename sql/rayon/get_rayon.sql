794ff14f7eec46bdec51fe5db929430066231105
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-14 13:14:08 +0800

    Add get rayon sql file

---
+++ sql/rayon/get_rayon.sql
@@ -0,0 +1,47 @@
+/* -------------------- THIS SQL FILE JUST FOR TEST QUERY ------------------- */
+SELECT
+  rayon.id_rayon,
+  rayon.nama,
+  -- JUMLAH KEPALA KELUARGA --
+  (
+    SELECT
+      COUNT(id_kk)
+    FROM
+      kepala_keluarga
+    WHERE
+      kepala_keluarga.id_rayon = rayon.id_rayon
+  ) AS jumlah_kk,
+  -- JUMLAH JEMAAT --
+  (
+    SELECT
+      COUNT(id_anggota_kk)
+    FROM
+      anggota_kk
+    WHERE
+      kepala_keluarga.id_rayon = rayon.id_rayon
+  ) AS jumlah_jemaat,
+  -- JUMLAH JEMAAT LAKI-LAKI --
+  (
+    SELECT
+      COUNT(id_anggota_kk)
+    FROM
+      anggota_kk
+    WHERE
+      kepala_keluarga.id_rayon = rayon.id_rayon
+      AND anggota_kk.jk = 'L'
+  ) AS jumlah_jemaat_l,
+  -- JUMLAH JEMAAT PEREMPUAN --
+  (
+    SELECT
+      COUNT(id_anggota_kk)
+    FROM
+      anggota_kk
+    WHERE
+      kepala_keluarga.id_rayon = rayon.id_rayon
+      AND anggota_kk.jk = 'P'
+  ) AS jumlah_jemaat_p
+FROM
+  rayon
+  LEFT JOIN kepala_keluarga ON rayon.id_rayon = kepala_keluarga.id_rayon
+GROUP BY
+  rayon.id_rayon;
