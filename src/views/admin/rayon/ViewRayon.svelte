61f90534ae9157d4ded8a2492ad0b81f40f0cdde
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-14 13:24:19 +0800

    Add more columns to view rayon page

--- src/views/admin/rayon/ViewRayon.svelte
+++ src/views/admin/rayon/ViewRayon.svelte
@@ -1,38 +1,33 @@
 <script>
-  import * as fetchService from '/@store/fetch.service.js'
-  import { link } from 'svelte-spa-router'
-  // import appConfig from '../../../../app.config.json'
+  import { fetch } from '/@store/fetch.service.js'
+  // import * as router from 'svelte-spa-router'
 
-  export let params
+  export let params = { id: 0 }
 
-  let loading = true
-  let currentRayon
+  let rayon = {}
 
-  fetchService
-    .fetch('/api/data/rayon')
-    .then((rayons) => {
-      currentRayon = rayons.find((rayon) => rayon.id_rayon == params.id)
-    })
-    .finally(() => {
-      loading = false
-    })
-
-  $: resolved = !loading && currentRayon
+  const rayonResponse = fetch(`/api/data/rayon/${params.id}`).then((_rayon) => {
+    rayon = _rayon
+    return _rayon
+  })
 </script>
 
-<!-- <svelte:head>
-  {#if resolved}
-    <title>Rayon {currentRayon.nama} - {appConfig.app_name}</title>
-  {/if}
-</svelte:head> -->
-
-<div class="flex flex-col min-h-full">
-  {#if resolved}
-    <h3 class="page-header">Rayon {currentRayon.nama}</h3>
-  {:else}
-    <div class="flex flex-col items-center justify-center w-full h-full p-3 text-gray-600 bg-white hidden-100 card">
-      <div class="text-lg">Uh oh... Tidak bisa menemukan rayon 😟</div>
-      <a class="link" href="/admin/rayon" use:link>Kembali ke halaman rayon</a>
-    </div>
-  {/if}
+<div class="flex flex-col flex-1 overflow-hidden bg-white card">
+  <div class="p-3 border-b">
+    <h3 class="text-lg">Rayon {rayon && rayon.nama}</h3>
+  </div>
+  <div class="flex flex-col flex-1 p-3 overflow-y-auto">
+    <!--  -->
+    {#await rayonResponse}
+      loading...
+    {:then rayon}
+      <!-- <pre>
+        {JSON.stringify(rayon)}
+      </pre> -->
+      <div>Jumlah Keluarga: {rayon.jumlah_kk}</div>
+      <div>Jumlah Jemaat: {rayon.jumlah_jemaat}</div>
+      <div>Jumlah Jemaat Laki-laki: {rayon.jumlah_jemaat_l}</div>
+      <div>Jumlah Jemaat Perempuan: {rayon.jumlah_jemaat_p}</div>
+    {/await}
+  </div>
 </div>
