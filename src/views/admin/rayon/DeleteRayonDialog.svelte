ca0b93d60ac406826721c491be1ca082b9070bb2
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-16 23:19:26 +0800

    Refactor delete rayon dialog

--- src/views/admin/rayon/DeleteRayonDialog.svelte
+++ src/views/admin/rayon/DeleteRayonDialog.svelte
@@ -10,30 +10,26 @@
   /** @type {Dialog} */
   let dialog
   let loading = false
-  let dataRayon = { nama: '' }
+  let rayon = { nama: '' }
   /** @type {import('axios').CancelTokenSource} */
   let cancelTokenSrc
 
-  export function open(_dataRayon) {
+  export function open(_rayon) {
     dialog.open()
-    dataRayon = _dataRayon
+    rayon = _rayon
   }
 
   function doDelete() {
     loading = true
     cancelTokenSrc = axios.CancelToken.source()
-    dataRayon &&
+    rayon &&
       http
-        .delete('/api/data/rayon/' + dataRayon.id_rayon)
-        .then((response) => {
-          if (response.status == 200) {
-            // preventDialogClosing = false
-            emit('success', { dataRayon, successType: 3 })
+        .delete('/api/data/rayon/' + rayon.id_rayon)
+        .then(({ status }) => {
+          if (status == 200) {
+            emit('success', { rayon, successType: 3 })
             dialog.close(true)
           }
-        })
-        .catch((err) => {
-          console.error(err)
         })
         .finally(() => (loading = false))
   }
@@ -49,31 +45,28 @@
 </script>
 
 <Dialog bind:this={dialog} class="simple-dialog" on:closed persistent>
-  <h3 class="px-3 py-2 text-lg">Hapus Rayon {dataRayon.nama}?</h3>
-  <div class="px-3 py-2">
-    Apakah kamu yakin akan menghapus data ini? Data yang telah dihapus tidak dapat dikembalikan.
-  </div>
-  {#if dataRayon.jumlah_kk > 0}
+  <h3 class="px-3 py-2 text-lg">Hapus Rayon</h3>
+  <div class="px-3 py-2">Apakah kamu yakin akan menghapus rayon {rayon.nama}?</div>
+  {#if rayon.jumlah_kk > 0}
     <div class="px-3 py-2 text-red-500">
-      Catatan: Rayon
-      {dataRayon.nama}
+      Peringatan: Rayon
+      {rayon.nama}
       memiliki
-      {dataRayon.jumlah_kk}
-      kepala keluarga, aksi ini juga akan menghapus data-data kepala keluarga tersebut.
+      {rayon.jumlah_kk}
+      keluarga, aksi ini akan menghapus semua keluarga yang ada!
     </div>
   {/if}
-  <!-- <hr /> -->
   <div class="flex flex-col px-3 py-2 overflow-y-auto" />
   <div class="flex px-3 py-2 mt-auto">
-    <Button on:click={cancel} icon="cancel">{loading ? 'batalkan' : 'tutup'}</Button>
+    <Button on:click={cancel} icon="cancel">{loading ? 'batalkan' : 'tidak'}</Button>
     <Button
-      class="ml-auto text-white bg-red-500 btn btn-scale"
+      class="ml-auto text-white btn btn-scale"
       style="background-color:#ff5959"
       disabled={loading}
       icon={loading || 'delete'}
       on:click={doDelete}
       title="Hapus Rayon">
-      {!loading ? 'Hapus' : 'loading...'}
+      {!loading ? 'hapus' : 'menghapus...'}
     </Button>
   </div>
 </Dialog>
