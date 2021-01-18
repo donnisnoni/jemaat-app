adbf34ed8ae45f85770191becabe164fee3f33a8
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-18 16:59:25 +0800

    Add iconOnly prop

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,13 +3,19 @@
   import { forwardEventsBuilder } from '/@actions/forwardEvents.js'
 
   export let icon = '',
-    primary = false
+    primary = false,
+    iconOnly = false
   // loading = false;
 
   const forwardEvents = forwardEventsBuilder(current_component)
 </script>
 
-<button class="btn" class:btn-primary={primary} use:forwardEvents {...$$props}>
+<button
+  class="btn"
+  class:btn-primary={primary}
+  class:icon-only={icon.length && iconOnly}
+  use:forwardEvents
+  {...$$props}>
   <!-- {#if loading}
     <span class="btn__loader">
       <ProgressCircular indeterminate size="23" width="2" />
