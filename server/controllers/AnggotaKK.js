fbe54bcb877fd43e215cf54cd3e0b0ac74ceb35e
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-19 18:31:43 +0800

    Add option to select fields

--- server/controllers/AnggotaKK.js
+++ server/controllers/AnggotaKK.js
@@ -7,12 +7,31 @@ const db = require('../models')
  */
 async function get(req, reply) {
   const qMetadata = req.query.metadata
+  const fields = req.query.fields && req.query.fields.length ? req.query.fields.split(',') : null
+  const allowedFields = ['jumlah_jemaat', 'jumlah_jemaat_l', 'jumlah_jemaat_p']
+
+  const getJumlahJemaat = () => db.anggota_kk.count()
+  const getJumlahJemaatL = () => db.anggota_kk.count({ where: { jk: 'L' } })
+  const getJumlahJemaatP = () => db.anggota_kk.count({ where: { jk: 'P' } })
 
   if (!!qMetadata) {
-    const metadata = {
-      jumlah_jemaat: await db.anggota_kk.count(),
-      jumlah_jemaat_l: await db.anggota_kk.count({ where: { jk: 'L' } }),
-      jumlah_jemaat_p: await db.anggota_kk.count({ where: { jk: 'P' } }),
+    let metadata = {}
+    if (fields) {
+      if (fields.includes(allowedFields[0])) {
+        metadata[allowedFields[0]] = await getJumlahJemaat()
+      }
+      if (fields.includes(allowedFields[1])) {
+        metadata[allowedFields[1]] = await getJumlahJemaatL()
+      }
+      if (fields.includes(allowedFields[2])) {
+        metadata[allowedFields[2]] = await getJumlahJemaatP()
+      }
+    } else {
+      metadata = {
+        jumlah_jemaat: await getJumlahJemaat(),
+        jumlah_jemaat_l: await getJumlahJemaatL(),
+        jumlah_jemaat_p: await getJumlahJemaatP(),
+      }
     }
     return reply.send(metadata)
   }
