e2d5b3e580261d18527cbf32f5357a4aedbf3df7
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-27 19:01:13 +0800

    Cleanup: Remove uneeded files; Improve fetch service

--- src/store/fetch.service.js
+++ src/store/fetch.service.js
@@ -1,27 +1,26 @@
 import { writable } from 'svelte/store'
-import cache from './cache'
 import http from '/@shared/http'
 // import axios from 'axios'
 
+const cache = new Map()
+
 // let cancelTokenSrc = axios.CancelToken.source()
 
-export function fetch(url, onSuccess, onDone) {
+export function fetch(url, onSuccess, onDone, onError) {
   const store = writable(new Promise(() => {}))
   if (cache.has(url)) {
     store.set(Promise.resolve(cache.get(url)))
   }
 
-  const load = async () => {
-    const response = await http.get(url)
-    const data = await response.data
-    cache.set(url, data)
-    store.set(Promise.resolve(data))
-    return Promise.resolve(data)
-  }
-
-  load()
-    .then((data) => typeof onSuccess === 'function' && onSuccess(data))
-    .finally(() => typeof onDone === 'function' && onDone())
+  http
+    .get(url)
+    .then(({ data }) => {
+      cache.set(url, data)
+      store.set(Promise.resolve(data))
+      typeof onSuccess == 'function' && onSuccess(data)
+    })
+    .catch((err) => typeof onError == 'function' && onError(err))
+    .finally(() => typeof onDone == 'function' && onDone())
 
   return store
 }
