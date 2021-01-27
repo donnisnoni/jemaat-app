e0faf2d70e6a3a46682c5256a660bc3fbcf6027f
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-27 22:37:22 +0800

    Remove `ubah` caption on update mode

--- src/views/admin/kk/KK.svelte
+++ src/views/admin/kk/KK.svelte
@@ -126,7 +126,7 @@
 <div class="flex flex-col flex-1 overflow-hidden bg-white card">
   <div class="flex flex-col flex-wrap p-3 bg-white border-b border-gray-200 md:flex-row ">
     <div class="mb-2 md:place-self-center md:mb-0">
-      <h3 class="text-lg">{isUpdate ? 'Ubah' : ''} Keluarga</h3>
+      <h3 class="text-lg">Keluarga {KKPrototype.nama}</h3>
     </div>
     <div class="w-full border border-t md:hidden" />
     <div class="mt-2 ml-auto md:mt-0">
