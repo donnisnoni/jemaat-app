afb4683319cf6918de1b08ce423d7ec424eee508
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-27 19:40:37 +0800

    Bring back purgecss to purge unused material icons

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 const PRODUCTION = process.env.NODE_ENV == 'production'
-// const tailwindConfig = require('./tailwind.config.js')
+const tailwindConfig = require('./tailwind.config.js')
 
 module.exports = {
   plugins: [
+    PRODUCTION && require('@fullhuman/postcss-purgecss')(tailwindConfig.purge),
     PRODUCTION && require('postcss-import'),
     require('tailwindcss'),
-    // PRODUCTION && require('@fullhuman/postcss-purgecss')(tailwindConfig.purge),
     PRODUCTION && require('autoprefixer'),
     PRODUCTION && require('cssnano')(),
   ],
