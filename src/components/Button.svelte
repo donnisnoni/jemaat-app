bd751023d2a8422a91e3981105d0292763e11bd5
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-31 01:55:41 +0800

    Fix button take space when button is not a primary

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -22,7 +22,6 @@
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
       class="loading-icon"
-      width="100%"
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid">
       <circle
