69b06a0acabef32844d0da4846cb56e777647fc6
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-01-31 00:52:52 +0800

    Fix button loading

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -18,31 +18,29 @@
   use:forwardEvents
   {...$$props}>
   {#if loading}
-    <span class="btn__loader">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        class="p-3 loading-icon"
-        width="100%"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid">
-        <circle
-          cx="50"
-          cy="50"
-          fill="none"
-          stroke-width="10"
-          r="35"
-          stroke-dasharray="164.93361431346415 56.97787143782138">
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            repeatCount="indefinite"
-            dur="1s"
-            values="0 50 50;360 50 50"
-            keyTimes="0;1" />
-        </circle>
-      </svg>
-    </span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      class="loading-icon"
+      width="100%"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid">
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke-width="10"
+        r="35"
+        stroke-dasharray="164.93361431346415 56.97787143782138">
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1" />
+      </circle>
+    </svg>
   {:else}
     {#if icon.length}<i class="mdi mdi-{icon}" />{/if}
     <slot />
