9dbaadd8dbb7f1ede1b6a203136d935ef7e75935
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-03-28 00:10:40 +0800

    Migrate to  vite v2.1.3
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,45 @@
-import { svelte } from 'vite-plugin-svelte'
+const { defineConfig } = require('vite')
+import svelte from '@svitejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
 import { getAliases } from 'vite-aliases'
 
-const aliases = getAliases()
+const aliases = getAliases({ prefix: '/@' })
 const preprocess = sveltePreprocess({ postcss: true })
 
-/** @type {import('vite').UserConfig} */
-export default {
-  plugins: [svelte({ preprocess })],
-  rollupDedupe: ['svelte'],
-  alias: aliases,
-  optimizeDeps: {
-    exclude: [
-      'ajv-errors',
-      'bcrypt',
-      'dotenv',
-      'ejs',
-      'fastify-jwt',
-      'fastify',
-      'mariadb',
-      'pdf-lib',
-      'puppeteer',
-      'sequelize',
+module.exports = defineConfig(({ /* command, */ mode }) => {
+  const isProduction = mode === 'production'
+  return {
+    plugins: [
+      svelte({
+        hot: !isProduction,
+        emitCss: true,
+        preprocess,
+      }),
     ],
-  },
-  proxy: {
-    '/api': { target: 'http://localhost:8080', changeOrigin: true },
-  },
-}
+    build: {
+      minify: isProduction,
+    },
+    optimizeDeps: {
+      exclude: [
+        'ajv-errors',
+        'bcrypt',
+        'dotenv',
+        'ejs',
+        'fastify-jwt',
+        'fastify',
+        'mariadb',
+        'pdf-lib',
+        'puppeteer',
+        'sequelize',
+      ],
+    },
+    resolve: {
+      alias: aliases,
+    },
+    server: {
+      proxy: {
+        '/api': { target: 'http://localhost:8080', changeOrigin: true },
+      },
+    },
+  }
+})
